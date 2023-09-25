a1b14251fa96f483773f984ae3bcd84a45a46348
Gonzalo <Gonzalo@DESKTOP-R2C3DDO>
2023-09-25 15:17:16 -0300

    Agrego controlador UsersController. Los datos se generan directamente en el modelo del controlador, no se pueden ingresar por vista aún.

---
+++ CBTeam-Model-View-Controller/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using CBTeam_Model_View_Controller.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace CBTeam_Model_View_Controller.Controllers
+{
+    [Route("users")]
+    public class UsersController : Controller
+    {
+        [Route("obtener-todos")]
+        public IActionResult GetList()
+        {
+            var user = new UserModel
+            {
+                Name = "Tomás",
+                Surname = "Barrera"
+            };
+            return View("Index", user);
+        }
+    }
+}
---
+++ CBTeam-Model-View-Controller/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CBTeam_Model_View_Controller.Models
+{
+    public class UserModel
+    {
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+    }
+}
